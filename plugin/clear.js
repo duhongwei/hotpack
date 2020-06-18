fe50b7284ca59b875bf5f73ef649bd5b8070a577
duhongwei <757318006@qq.com>
2020-06-19 07:43:42 +0800

    save

--- plugin/clear.js
+++ plugin/clear.js
@@ -1,8 +1,11 @@
 export default async function ({ debug }) {
+  const { util: { isInclude,isMedia } } = this
   return async function (files) {
+    debug(files.map(file=>file.key))
     for (let file of files) {
-      if (this.isPro()) {
-        if (/\.(css|js)$/.test(file.key)) {
+      
+      if (this.isPro() && !isInclude(file.key)) {
+        if (/\.(css|js)$/.test(file.key)||isMedia(file.key)) {
           debug(`clear ${file.key}`)
           file.del = true
         }
--- plugin/key.js
+++ plugin/key.js
@@ -1,20 +1,9 @@
 
 export default async function ({ debug, opt: { name } }) {
-  const { config: { src } } = this
+  
   return function (files) {
     for (let file of files) {
-
-      let key = file.path.replace(src, '').split(/[/\\]/).join('/').substr(1)
-      if (/\/node_modules\//.test(file.path)) {
-        const pathList = file.path.split('/')
-        let nodeKey = pathList[pathList.indexOf('node_modules') + 1]
-
-        key = `node/${nodeKey}.js`
-
-      }
-      file.key = key
-      debug(`${file.path} =>  ${key}`)
+      file.key = this.resolvePath(file.path)
     }
-
   }
 }
