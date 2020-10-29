1cef8cb5444355da827d9e0f107cf58c7297c9bd
duhongwei <757318006@qq.com>
2020-10-29 17:26:27 +0800

    add system plugin env

---
+++ plugin/env.js
@@ -0,0 +1,13 @@
+export default async function ({ debug }) {
+  const { util: { isJs } } = this
+  this.on('afterSlim', function (files) {
+    for (let file of files) {
+      if (!isJs(file.key)) continue
+      debug(`env ${file.key}`)
+
+      file.content = file.content.replace(/process.env.NODE_ENV/g, `"${process.env.NODE_ENV}"`)
+      file.content = file.content.replace(/process.env.test/g, `"${process.env.test}"`)
+    }
+
+  })
+}
