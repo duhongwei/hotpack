c1e9144c72722193704d755c8044b53092d05fb3
duhongwei <757318006@qq.com>
2020-12-24 17:55:50 +0800

    add file ignore

--- plugin/file.js
+++ plugin/file.js
@@ -1,13 +1,13 @@
 
 export default async function ({ debug }) {
   return async function () {
-    
+    let {config } = this
     if (this.isHot() && this.hotPath) {
       this.filePaths = [this.hotPath]
     }
     else {
-      let paths = await this.fs.readFilePath()
-      this.filePaths = this.filePaths.concat(paths)
+      let paths = await this.fs.readFilePath(config.ignoreFunc)
+      this.filePaths = this.filePaths.concat(paths) 
     }
     debug(`read ${this.filePaths.length} filePaths `)
     debug(this.filePaths)
