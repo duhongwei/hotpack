d533d3830c5c31baf917e6f9f2ecbde93a8ba1a6
duhongwei <757318006@qq.com>
2021-02-02 10:17:33 +0800

    delete file keys and deps from version if file is deleted

--- plugin/remove.js
+++ plugin/remove.js
@@ -1,6 +1,8 @@
 /**
  * remove的意义，清洁是次要的，主要是为了不需要全新编译也可以发现import的文件已经丢失
  * 
+ * 不光是要删除没有的文件，还要从version.json中删除相应的key，和相应的依赖
+ * 
  */
 export default async function ({ debug }) {
 
@@ -11,6 +13,8 @@ export default async function ({ debug }) {
     for (let file of files) {
       fileSet.add(file.key)
     }
+
+    
     let versionData = this.version.get()
     let keys = Object.keys(versionData)
 
@@ -18,7 +22,9 @@ export default async function ({ debug }) {
       if (/^(other|runtime)\//.test(key)) continue
    
       if (!fileSet.has(key)) {
-        delete versionData[key]
+     
+        //删除这个key和相关的依赖
+        this.version.delKeyAndDeps(key)
         debug(`del key ${key} `)
         continue
       }
