60a12e5087a96c16725b1a19db64f0519a8f8746
duhongwei <duhongwei@360.cn>
2021-07-01 17:54:42 +0800

    readable

--- plugin/read.js
+++ plugin/read.js
@@ -1,10 +1,7 @@
 export default async function ({ debug }) {
   return async () => {
-    let files = await this.fs.read(this.filePaths)
-    for (let file of files) {
-
-      this.addFile(file)
-    }
+    //为 this.files 的每个 file 补充 content
+    await this.fs.read(this.files)
 
     debug(`read ${this.files.length} files `)
     debug(this.files.map(file => file.path))
