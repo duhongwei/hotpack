bd164be92349f909de2fa68464327e2a96944c30
duhongwei <duhongwei@360.cn>
2021-07-04 15:08:45 +0800

    fix

--- plugin/read.js
+++ plugin/read.js
@@ -1,8 +1,10 @@
+
+
 export default async function ({ debug }) {
   return async () => {
     //为 this.files 的每个 file 补充 content
     await this.fs.read(this.files)
-
+    
     debug(`read ${this.files.length} files `)
     debug(this.files.map(file => file.path))
   }
--- plugin/ssrImport.js
+++ plugin/ssrImport.js
@@ -1,20 +1,21 @@
-import { dealImport, isServerFile } from "../lib/ssr.js"
+import { dealImport, isServerFile, getSsrFile } from "../lib/ssr.js"
+import { join } from 'path'
 /**
  * 已经完成 预渲染了，把需要发布的文件写操作，覆盖原来用于预渲染的文件 
  */
 export default async function () {
-    const { config } = this
-    
+    const { config, fs } = this
+
 
     return async function (files) {
         if (!config.renderEnabled) return
 
         if (config.render.publishPath) {
             let publishFiles = getSsrFile(files)
-            let files = await dealImport(publishFiles, config.render.publishPath)
-            await save(files)
+            let fileList = await dealImport(publishFiles, config.render.publishPath)
+            await save(fileList)
         }
-        
+
         //从 files中 删除 服务端文件，避免后面的 browser端的解析出错
         for (let file of files) {
             if (isServerFile(file)) {
