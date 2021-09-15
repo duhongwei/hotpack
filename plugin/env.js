2b6875356197e24b866ff1d03a54245a1c3983d0
duhongwei <duhongwei@360.cn>
2021-09-15 14:21:21 +0800

    fix bug. afterUseImage replace afterslim

--- plugin/env.js
+++ plugin/env.js
@@ -1,6 +1,7 @@
 export default async function ({ debug }) {
   const { util: { isJs } } = this
-  this.on('afterSlim', function (files) {
+  //The afterslim event is not appropriate. If the content of the file has not changed, the changes in process.env will not be applied to the file
+  this.on('afterUseImage', function (files) {
     for (let file of files) {
       if (!isJs(file.key)) continue
       debug(`env ${file.key}`)
